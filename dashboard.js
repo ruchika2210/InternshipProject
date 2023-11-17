828f22c3a4826d9e3a8244385ab8b98cb271ece4
chandr15 <chandr15@uwindsor.ca>
2023-11-17 15:17:20 -0500

    Aditi | Dashboard | chart generation

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,168 @@
+// Function to render a bar chart
+function renderBarChart(categoryData) {
+    const ctx = document.getElementById('barChart').getContext('2d');
+
+    // Extract data for the chart
+    const labels = categoryData.categories.map(category => category.name);
+    const weights = categoryData.categories.map(category => category.weight);
+
+    // Bar chart data
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Category Weights',
+            data: weights,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    };
+
+    // Bar chart options
+    const options = {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
+    // Create the bar chart
+    const barChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+}
+
+// Function to render a pie chart
+function renderPieChart(scenarioData) {
+    const ctx = document.getElementById('pieChart').getContext('2d');
+
+    // Extract data for the chart
+    const labels = scenarioData.scenarios.map(scenario => scenario.emissionsReduction);
+    const dataValues = scenarioData.scenarios.map(scenario => scenario.jobCreated);
+
+    // Pie chart data
+    const data = {
+        labels: labels,
+        datasets: [{
+            data: dataValues,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+            ],
+            borderWidth: 1
+        }]
+    };
+
+    // Pie chart options
+    const options = {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
+    // Create the pie chart
+    const pieChart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: options
+    });
+}
+
+// Retrieve data from Category Input Table
+const categoryData = {
+    categories: []
+};
+
+document.querySelectorAll('#categoryInput .data-table tbody tr').forEach(row => {
+    const category = {
+        name: row.querySelector('.category-name')?.value,
+        weight: row.querySelector('.category-weight')?.value,
+        direction: row.querySelector('.category-direction')?.value,
+        evaluationType: row.querySelector('.category-evaluation-type')?.value
+    };
+    console.log(category);  // Add this line
+
+    categoryData.categories.push(category);
+});
+
+console.log(categoryData);  // Add this line
+
+
+// Retrieve data from Scenario Input Table
+const scenarioInputData = {
+    scenarios: []
+};
+
+document.querySelectorAll('#scenarioInput .data-table tbody tr').forEach(row => {
+    const scenario = {
+        name: row.querySelector('.scenario-name')?.value
+    };
+
+    console.log(scenario);  // Add this line
+
+    scenarioInputData.scenarios.push(scenario);
+});
+
+console.log(scenarioInputData);  // Add this line
+
+// Retrieve data from Scenario Data Table
+const scenarioData = {
+    scenarios: []
+};
+
+document.querySelectorAll('#scenarioData .data-table tbody tr').forEach(row => {
+    const emissionsReduction = row.querySelector('.emissions-reduction')?.value;
+    const airQualityImpact = row.querySelector('.air-quality-impact')?.value;
+    const wildlifeDisturbance = row.querySelector('.wildlife-disturbance')?.value;
+    const incomeGeneration = row.querySelector('.income-generation')?.value;
+    const jobCreated = row.querySelector('.job-created')?.value;
+
+    // Check if all values are present before adding to scenarios
+    if (emissionsReduction && airQualityImpact && wildlifeDisturbance && incomeGeneration && jobCreated) {
+        const scenario = {
+            emissionsReduction,
+            airQualityImpact,
+            wildlifeDisturbance,
+            incomeGeneration,
+            jobCreated
+        };
+        console.log(scenario);  // Add this line
+
+        scenarioData.scenarios.push(scenario);
+    }
+});
+
+console.log(scenarioData);  // Add this line
+
+// Store data in localStorage
+localStorage.setItem('categoryData', JSON.stringify(categoryData));
+localStorage.setItem('scenarioInputData', JSON.stringify(scenarioInputData));
+localStorage.setItem('scenarioData', JSON.stringify(scenarioData));
+
+
+console.log(categoryData);
+console.log(scenarioData);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    renderBarChart(categoryData);
+    renderPieChart(scenarioData);
+});
+
+//Form submitted and stored in local storage
 function submitCategoryData() {
     const categoryData = {
         categories: []
@@ -28,7 +193,7 @@ function submitScenarioInput() {
             name: row.querySelector('.scenario-name')?.value
         };
 
-        //console.log(scenario);
+        // console.log(scenario);
 
         scenarioInputData.scenarios.push(scenario);
     });
@@ -36,7 +201,6 @@ function submitScenarioInput() {
     localStorage.setItem('scenarioInputData', JSON.stringify(scenarioInputData));
     alert("Scenario input submitted!");
 }
-
 
 function submitScenarioData() {
     const scenarioData = {
@@ -51,8 +215,8 @@ function submitScenarioData() {
             incomeGeneration: row.querySelector('.income-generation')?.value,
             jobCreated: row.querySelector('.job-created')?.value
         };
-        
-        console.log(scenario);
+
+        // console.log(scenario);
 
         scenarioData.scenarios.push(scenario);
     });
@@ -60,7 +224,6 @@ function submitScenarioData() {
     localStorage.setItem('scenarioData', JSON.stringify(scenarioData));
     alert("Scenario data submitted!");
 }
-
 
 document.getElementById('submitCategoryButton').addEventListener('click', submitCategoryData);
 document.getElementById('submitScenarioInputButton').addEventListener('click', submitScenarioInput);
